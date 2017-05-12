124a786d1d10adef353cc71b1b4899bcbb6d8b62
snowyjone <w007878@hotmail.com>
2017-05-13 01:17:30 +0800

    load_data

---
+++ train.py
@@ -0,0 +1,13 @@
+import keras
+import os
+import cv2
+import argparse
+
+def load_img(path):
+	img = cv2.imread(path, 1)
+
+def load_args():
+	parser = argparse.ArgumentParser(description = 'train the GAN model')
+
+if __name__ == '__main__':
+	load_args()	
