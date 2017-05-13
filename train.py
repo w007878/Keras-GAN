260642ba9358500cf4b5d7273497c8ade5a47312
snowyjone <w007878@hotmail.com>
2017-05-13 12:26:23 +0800

    train

--- train.py
+++ train.py
@@ -1,11 +1,17 @@
 import keras
 import cv2
 
+import numpy as np
+
 from load import load_args
 from load import load_img
 
+del train(PATH, batch, epochs):
+	img = load_img(PATH)
+	
 if __name__ == '__main__':
 	args = load_args()	
 	print args
 
-	img = load_img(args.PATH)
+	train(args.PATH, args.batch, args.epochs)
+
