c697fe158fc5937fdccd55fb10e8b2f4736c1880
snowyjone <w007878@hotmail.com>
2017-05-14 22:31:54 +0800

    update model

--- model.py
+++ model.py
@@ -4,13 +4,24 @@ import numpy as np
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.layers.convolutional import Conv2D
 
-def generater_model():
+def generator_model():
 	model = Sequential()
-	model.add(Dense(32, input_dim = 32 * 32))
+	model.add(Dense(32, input_dim = 100))
+	model.add(Activation('sigmoid'))
+	model.add(Dense(64))
+	model.add(Activation('relu'))
+
 	return model
 
 def discriminator_model():
 	model = Sequential()
+	return model
+
+def adversial_model(generator, discriminator):
+	model = Sequential()
+	model.add(generator)
+	model.add(discriminator)
 	return model
 
