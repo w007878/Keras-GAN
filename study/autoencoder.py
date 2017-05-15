7e45a9b5307849ffa8c4bd60da34d165b2ac6900
snowyjone <w007878@hotmail.com>
2017-05-15 19:52:42 +0800

    update autoencoder

--- study/autoencoder.py
+++ study/autoencoder.py
@@ -25,11 +25,21 @@ decoder = Model(encoded_input, decoder_layer(encoded_input))
 autoencoder.compile(optimizer = 'sgd', loss = 'binary_crossentropy')
 
 (x_train, _), (x_test, _) = mnist.load_data()
+print x_train.shape
+print np.prod(x_train.shape[1:])
+print np.prod(x_train.shape)
+
+x_train = x_train.astype('float32') / 255
+x_test = x_train.astype('float32') / 255
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
 print x_train.shape
 print np.prod(x_train.shape[1:])
 print np.prod(x_train.shape)
 
+
+autoencoder.fit(x_train, x_train, epochs = 50, batch_size = 32, shuffle = True, validation_data = (x_test, x_test))
 print encoding_dim 
 
 
