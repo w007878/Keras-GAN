c18d9bcf0547fe567bc2b1373cbe1dc488bd6d7a
snowyjone <w007878@hotmail.com>
2017-05-15 23:39:58 +0800

    finished but need to upgrade

--- study/autoencoder.py
+++ study/autoencoder.py
@@ -7,7 +7,7 @@ from keras.models import Model
 from keras.datasets import mnist
 import matplotlib.pyplot as plt
 
-encoding_dim = 32
+encoding_dim = 256
 
 input_img = Input(shape = (784, ))
 
@@ -23,24 +23,21 @@ decoder_layer = autoencoder.layers[-1]
 
 decoder = Model(encoded_input, decoder_layer(encoded_input))
 
-autoencoder.compile(optimizer = 'sgd', loss = 'binary_crossentropy')
+autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
 
 (x_train, _), (x_test, _) = mnist.load_data()
 print x_train.shape
 print np.prod(x_train.shape[1:])
 print np.prod(x_train.shape)
 
-x_train = x_train.astype('float32') / 255
-x_test = x_train.astype('float32') / 255
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
 print x_train.shape
-print np.prod(x_train.shape[1:])
-print np.prod(x_train.shape)
 
-
-autoencoder.fit(x_train, x_train, epochs = 50, batch_size = 32, shuffle = True, validation_data = (x_test, x_test))
+autoencoder.fit(x_train, x_train, epochs = 50, batch_size = 256, shuffle = True, validation_data = (x_test, x_test))
 
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
