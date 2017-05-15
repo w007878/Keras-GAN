fbf91eef55a0c6df8b3825f466b8e10a9fb5a641
snowyjone <w007878@hotmail.com>
2017-05-15 09:21:10 +0800

    update train

--- train.py
+++ train.py
@@ -6,8 +6,17 @@ import numpy as np
 from load import load_args
 from load import load_img
 
-del train(PATH, batch, epochs):
+def train(PATH, batch, epochs):
 	img = load_img(PATH)
+	for epoch in range(epochs):
+		if(epoch == 0):
+				if os.path.exist('generator_weights') ans os.path.exist('discriminator_weights'):
+					print "Loading Saved Weights.."
+					generator.load_weights('generator_weights')
+					discriminator.load_weights('discriminator_weights')
+					print "Finished Loading"
+					pass
+
 	
 if __name__ == '__main__':
 	args = load_args()	
